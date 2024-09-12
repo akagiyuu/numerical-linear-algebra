85416a3ae3b2d0c9d6e0210321e3a991a5eeafe1
akagiyuu <khanghuynh0245@gmail.com>
2024-09-12 18:09:05 +0700

    feat: add vector functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::{
     fmt::Display,
+    iter::Sum,
     ops::{Add, AddAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign},
 };
 
-use num_complex::Complex64;
+use num_complex::{Complex64, ComplexFloat};
 use tabled::{
     builder::Builder,
     settings::{Margin, Style},
@@ -21,6 +22,14 @@ impl<const M: usize, const N: usize> Matrix<M, N> {
     }
 
     pub const ZEROS: Self = Matrix::new([[Complex64::ZERO; N]; M]);
+
+    pub fn iter(&self) -> impl Iterator<Item = &Complex64> {
+        self.data.iter().flatten()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Complex64> {
+        self.data.iter_mut().flatten()
+    }
 }
 
 impl<const M: usize> Matrix<M, M> {
@@ -54,11 +63,9 @@ impl<const M: usize, const N: usize> IndexMut<[usize; 2]> for Matrix<M, N> {
 
 impl<const M: usize, const N: usize> AddAssign for Matrix<M, N> {
     fn add_assign(&mut self, rhs: Self) {
-        for i in 0..M {
-            for j in 0..N {
-                self[[i, j]] += rhs[[i, j]];
-            }
-        }
+        self.iter_mut()
+            .zip(rhs.iter())
+            .for_each(|(left, right)| *left += right);
     }
 }
 
@@ -72,13 +79,17 @@ impl<const M: usize, const N: usize> Add for Matrix<M, N> {
     }
 }
 
+impl<const M: usize, const N: usize> Sum for Matrix<M, N> {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.reduce(|acc, x| acc + x).unwrap_or(Matrix::ZEROS)
+    }
+}
+
 impl<const M: usize, const N: usize> SubAssign for Matrix<M, N> {
     fn sub_assign(&mut self, rhs: Self) {
-        for i in 0..M {
-            for j in 0..N {
-                self[[i, j]] -= rhs[[i, j]];
-            }
-        }
+        self.iter_mut()
+            .zip(rhs.iter())
+            .for_each(|(left, right)| *left -= right);
     }
 }
 
@@ -112,11 +123,7 @@ impl<const M: usize, const N: usize, const P: usize> Mul<Matrix<N, P>> for Matri
 
 impl<const M: usize, const N: usize> MulAssign<Complex64> for Matrix<M, N> {
     fn mul_assign(&mut self, rhs: Complex64) {
-        for i in 0..M {
-            for j in 0..N {
-                self[[i, j]] *= rhs;
-            }
-        }
+        self.iter_mut().for_each(|x| *x *= rhs);
     }
 }
 
@@ -127,6 +134,14 @@ impl<const M: usize, const N: usize> Mul<Complex64> for Matrix<M, N> {
         let mut result = self.clone();
         result *= rhs;
         result
+    }
+}
+
+impl<const M: usize, const N: usize> Mul<Matrix<M, N>> for Complex64 {
+    type Output = Matrix<M, N>;
+
+    fn mul(self, rhs: Matrix<M, N>) -> Self::Output {
+        rhs * self
     }
 }
 
@@ -141,5 +156,52 @@ impl<const M: usize, const N: usize> Display for Matrix<M, N> {
         table.with(Margin::new(4, 0, 0, 0));
 
         write!(f, "[\n{}\n]", table)
+    }
+}
+
+pub type Vector<const N: usize> = Matrix<1, N>;
+
+impl<const N: usize> Vector<N> {
+    pub fn dot(&self, rhs: &Vector<N>) -> Complex64 {
+        self.iter().zip(rhs.iter()).map(|(a, b)| a * b).sum()
+    }
+
+    pub fn norm(&self, p: u32) -> f64 {
+        self.data[0]
+            .iter()
+            .map(|x_i| x_i.abs().powi(p as i32))
+            .sum::<f64>()
+            .powf(1. / p as f64)
+    }
+
+    pub fn normalize(self) -> Self {
+        let length = Complex64::new(self.norm(2), 0.);
+        1. / length * self
+    }
+
+    pub fn project(self, rhs: Self) -> Self {
+        self.dot(&rhs) / rhs.dot(&rhs) * rhs
+    }
+
+    pub fn gram_schimidt_process<const M: usize>(vectors: [Vector<N>; M]) -> [Vector<N>; M] {
+        let mut basis_vectors = vectors.into_iter().enumerate().fold(
+            [Vector::<N>::ZEROS; M],
+            |mut basis_vectors, (i, vector)| {
+                basis_vectors[i] = vector.clone()
+                    - basis_vectors
+                        .iter()
+                        .take(i)
+                        .map(|basis_vector| vector.clone().project(basis_vector.clone()))
+                        .sum();
+
+                basis_vectors
+            },
+        );
+
+        basis_vectors
+            .iter_mut()
+            .for_each(|vector| *vector = vector.clone().normalize());
+
+        basis_vectors
     }
 }
